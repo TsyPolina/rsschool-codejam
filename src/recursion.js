e3064371e89cae042f655843642f6f9e2a2a666e
TsyPolina <tsy.polina@gmail.com>
2018-10-28 19:34:43 +0300

    feat(function): add recursion

---
+++ src/recursion.js
@@ -0,0 +1,20 @@
+module.exports = function recursion(tree, deep = 0, result = []) { 
+    const res = result; 
+    if (!tree.value) { 
+        return []; 
+    } 
+    const currentDeep = deep + 1; 
+    if (res.length < currentDeep) { 
+        res.push([tree.value]); 
+    } 
+    else { 
+        res[deep] = res[deep].concat(tree.value); 
+    } 
+    if (tree.left) { 
+        recursion(tree.left, currentDeep, result); 
+    } 
+    if (tree.right) { 
+        recursion(tree.right, currentDeep, result); 
+    } 
+    return res; 
+};
